10ee90f2deaa469f3cc46b19ecc0ed4e36226ebd
monarch <meyers@geospaces.org>
2020-09-26 08:24:55 -0600

    Commiting

---
+++ test/h.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+import mangorest.mango
+from   mangorest.mango import webapi
+
+@webapi("/app1/testingroot2/")
+def test2(request, h="test2", **kwargs):
+    ret = (f"Hello Test2 {h} {kwargs} {request.headers}")
+    ret = (f"Hello Test2 {h} {kwargs}")
+
+    print(ret)
+    return ret
+
+mangorest.mango.__VERSION__ = "1.1"
+mangorest.mango.PORT        = 9001
+if __name__ == '__main__' and not mangorest.mango.inJupyter():
+    print(f'** NOTICE *** registered URLS: {mangorest.mango._WEBAPI_ROUTES.keys()}')
+    mangorest.mango.main()
+    pass
