1723b98121ddee7f5de05b9f34471a9df62407ff
e346104 <sadananda.narayanappa@lmco.com>
2025-06-02 08:33:22 -0600

    Changes added

--- mangorest/mango.py
+++ mangorest/mango.py
@@ -128,10 +128,10 @@ def CallMethod(method, request, args=None):
     response = HttpResponse(ret)        
     response.headers["Access-Control-Allow-Origin"] = "*"
             
-    print(f"""
-          ****** Retruning *****
-          {response.headers["Access-Control-Allow-Origin"] }
-          """)
+    #print(f"""
+    #      ****** Retruning *****
+    #      {response.headers["Access-Control-Allow-Origin"] }
+    #      """)
     return response #, content_type="text/plain")
 #--------------------------------------------------------------------------------
 def TryRunPyMethod(request):
